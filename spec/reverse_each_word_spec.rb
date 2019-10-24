39fe9885c6bb445b41d15e9867a8d15541eaeef1
eunice kim <oonis86@gmail.com>
2019-10-24 20:21:00 +0000

    Automatically backed up by Learn

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
+  
    string = string.split(" ")
       newString = string.collect{|reverse_string| reverse_string.reverse}
-  #end
+      
   return newString.join(' ')
 end
