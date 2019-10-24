577c08f26917e50329d1a590470dd2a166fb99d7
eunice kim <oonis86@gmail.com>
2019-10-24 20:20:13 +0000

    Automatically backed up by Learn

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -15,21 +15,4 @@ describe '#reverse_each_word' do
     expect_any_instance_of(Array).to receive(:collect).and_call_original
     reverse_each_word("Verifying that collect is being called.")
   end
-
-
-def reverse_each_word(string)
-   string = string.split(" ")
-    #i = 0
-    #j = 0
-    #newString = [" "]
-    #while(i < string.length)
-    #string.each do |string|
-     #string = string.reverse()
-     #newString[j] = string
-       #i += 1
-       #j += 1
-    # end
-      newString = string.collect{|reverse_string| reverse_string.reverse}
-  #end
-  return newString.join(' ')
 end
